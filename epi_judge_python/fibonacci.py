16b7379f4e246dcdbfe220dff0da4804e2a1fccf
krisr <kris.ragin@outlook.com>
2025-02-02 03:41:06 -0600

    Initial fib implementation from memory

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -3,7 +3,17 @@ from test_framework import generic_test
 
 def fibonacci(n: int) -> int:
     # TODO - you fill in here.
-    return -1
+    curr = 1
+    prev = 1
+    if n == 0:
+        return 0
+    if n == 1:
+        return curr
+    for i in range(2, n):
+
+        curr, prev = curr + prev, curr
+
+    return curr
 
 
 if __name__ == '__main__':
